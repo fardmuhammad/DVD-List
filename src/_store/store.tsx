5dd116f857dab34981da5c1f85ea18956d2bf4dc
Fard Muhammad <heyitsfard@gmail.com>
2023-07-14 01:03:02 -0700

    Add files via upload
    Prettier errors.

--- src/_store/store.tsx
+++ src/_store/store.tsx
@@ -2,9 +2,9 @@ import { configureStore } from '@reduxjs/toolkit';
 import listReducer from '../features/list/listSlice';
 
 export const store = configureStore({
-    reducer: {
-        lists: listReducer,
-    }
+	reducer: {
+		lists: listReducer,
+	},
 });
 
 export default store;
